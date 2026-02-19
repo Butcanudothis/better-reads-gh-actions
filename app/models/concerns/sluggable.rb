756302e9672cc3af936657cb51aeb8b5546f4d06
butcanudothis <51348806+Butcanudothis@users.noreply.github.com>
2026-02-19 22:41:24 +1000

    Revert "Fix: apply rubocop autocorrect to sluggable.rb"
    This reverts commit c4a46f15b33a3d50f321b9f3392585662b63b44f.

--- app/models/concerns/sluggable.rb
+++ app/models/concerns/sluggable.rb
@@ -6,15 +6,15 @@ module Sluggable
   end
 
   def generate_slug
-    return unless title.present?
-
-    self.slug = title.parameterize
-    return unless self.class.where(slug: slug).where.not(id: id).exists?
-
-    self.slug = slug + '-' + SecureRandom.hex(3)
+    if self.title.present?
+      self.slug = self.title.parameterize
+      if self.class.where( slug: self.slug ).where.not( id: self.id ).exists?
+        self.slug = self.slug + "-" + SecureRandom.hex(3)
+      end
+    end
   end
 
   def to_param
-    slug
+    self.slug
   end
 end
